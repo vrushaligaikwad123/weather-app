af3a3a71d3ce92531d3dedd78fcd50d23c8b8368
vrushaligaikwad123 <2001vrushaligaikwad@gmail.com>
2023-10-08 19:12:30 +0530

    Initial commit

---
+++ src/views/Home/Home.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import "./Home.css";
+import axios from "axios";
+function Home(){
+    const[city, setCity] = useState('Pune')
+    const[temperature, setTemperature] = useState(0)
+    const[message, setMessage] = useState('');
+    async function loadWeatherInfo(){
+        try{
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f652964084c552e8c0492237a3fabd9c`)
+        setTemperature((response.data.main.temp - 273).toFixed(2));
+        setMessage('☑ Data fetched successfully...');
+        }
+        catch(err){
+            setTemperature(0);
+            setMessage('City not found');
+        }
+    }
+    useEffect(()=>{
+        loadWeatherInfo()
+    }, [])
+    return(
+        <div>
+            <h1 className="app-title">Weather For {city}</h1>
+            <input type="text" 
+            className="search-bar" 
+            placeholder="Enter City"
+            value={city}
+            onChange={(e)=>
+            setCity(e.target.value)
+            }
+            />
+            <p className="msg-text"> {message} </p>
+            <h1 className="temperature-text">Temperature: {temperature} °C</h1>
+            
+        </div>
+    )
+}
+export default Home
